4bc1954bdb56dc0f38ece18ea1dfbe635c0f5363
alk-hbp <145958110+alk-hbp@users.noreply.github.com>
2023-09-25 15:53:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,8 @@ streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭自分でフルーツスムージーを作ろう🥝🍇')
+
+
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.datafram(my_fruit_list)
