81e9ae8aba7d2e2f5794e10a825d00a2b44efb17
alk-hbp <145958110+alk-hbp@users.noreply.github.com>
2023-09-25 16:02:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,4 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index)) 
 # ページにテーブルを表示します。
 streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
