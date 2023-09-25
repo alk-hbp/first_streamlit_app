8a804c743efe576ae34daf36353b225c7e17b1bb
alk-hbp <145958110+alk-hbp@users.noreply.github.com>
2023-09-25 16:01:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.header('🍌🥭自分でフルーツスムージーを作ろう🥝�
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
 # ここに選択リストを置き、含めたい果物を選択できるようにしましょう。
 streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index)) 
 # ページにテーブルを表示します。
