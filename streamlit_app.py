3247eceb331f978ba8072442d2b9437fa504c8df
alk-hbp <145958110+alk-hbp@users.noreply.github.com>
2023-09-25 16:11:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,14 @@ streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 
-streamlit.header('🍌🥭自分でフルーツスムージーを作ろう🥝🍇')
+streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
 
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# ここに選択リストを置き、含めたい果物を選択できるようにしましょう。
+# ここに選択リストを置き、含めたい果物を選択できるようにしましょう。アボカド、いちごは例として表示
 streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) 
 # ページにテーブルを表示します。
 streamlit.dataframe(my_fruit_list)
