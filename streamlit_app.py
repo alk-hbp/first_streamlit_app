8c104ebc5ca4992452742e5e17ad3d54f89919a2
alk-hbp <145958110+alk-hbp@users.noreply.github.com>
2023-09-25 15:47:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('🥣Breakfast Menu')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭自分でフルーツスムージーを作ろう🥝🍇')
